1e4afdca7319287b737748f26ea0c738362f592f
batman9910021 <88237683+batman9910021@users.noreply.github.com>
2022-12-09 17:54:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,9 +12,9 @@ from bypasser import ddllist
 
 
 # bot
-bot_token = os.environ.get("TOKEN", "")
-api_hash = os.environ.get("HASH", "") 
-api_id = os.environ.get("ID", "")
+bot_token = os.environ.get("TOKEN", "5855800728:AAFNumvlGQuCBI6g9ZvWN2iiZcIqIv-AHpU")
+api_hash = os.environ.get("HASH", "dcb6bfd6d51a8ff5f6aadb01b9fdd11b") 
+api_id = os.environ.get("ID", "21879629")
 app = Client("my_bot",api_id=api_id, api_hash=api_hash,bot_token=bot_token)  
 
 
@@ -54,7 +54,7 @@ def loopthread(message):
 @app.on_message(filters.command(["start"]))
 def send_start(client: pyrogram.client.Client, message: pyrogram.types.messages_and_media.message.Message):
     app.send_message(message.chat.id, f"__👋 Hi **{message.from_user.mention}**, i am Link Bypasser Bot, just send me any supported links and i will you get you results.\nCheckout /help to Read More__",
-    reply_markup=InlineKeyboardMarkup([[ InlineKeyboardButton("🌐 Source Code", url="https://github.com/bipinkrish/Link-Bypasser-Bot")]]), reply_to_message_id=message.id)
+    reply_markup=InlineKeyboardMarkup([[ InlineKeyboardButton("🌐 Source Code", url="https://t.me/AKImaxPremium")]]), reply_to_message_id=message.id)
 
 
 # help command
